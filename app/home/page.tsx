ca976b597dc0aa83cef10724ed5d00b8416b410f
yazid koubci <koubciyazid@yahoo.fr>
2025-06-26 17:55:23 +0100

    correction  home page non fonctionnel

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,34 @@
+import React from "react";
+import mysql, { RowDataPacket } from "mysql2/promise";
+
+type MessageRow = {
+  message: string;
+};
+
+async function getMessageFromDB(): Promise<string> {
+  const connection = await mysql.createConnection({
+    host: '91.204.209.8',       // ex: sql.ahlemkoubci.icu
+    user: 'ahlemkou_vercel',       // ex: ahlemkoubci_user
+    password: 'Pomaria121165',
+    database: 'ahlemkou_tresorerie',
+    port: 3306
+  });
+
+  const [rows] = await connection.execute<RowDataPacket[] & MessageRow[]>(
+    "SELECT message FROM test_connection LIMIT 1"
+  );
+
+  await connection.end();
+
+  return rows.length > 0 ? rows[0].message : "Aucun message trouvé";
+}
+
 export default async function Home() {
+  const message = await getMessageFromDB();
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold text-center text-blue-600">test</h1>
+      <h1 className="text-4xl font-bold text-center text-blue-600">{message}</h1>
     </main>
   );
 }
