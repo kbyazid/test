7c8124315a4a1d89628ea0ebe3594d6015ed9fe6
yazid koubci <koubciyazid@yahoo.fr>
2025-06-26 16:47:42 +0100

    Nouveau test jout de la connexion

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     modific ation Test Deploy nextjs 
+     modific ation Test Deploy nextjs ajout de l api
     </div>
   );
 }
