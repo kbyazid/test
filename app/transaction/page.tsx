0569db1bd3f7003e3aabce397061b68e95492a2d
yazid koubci <koubciyazid@yahoo.fr>
2025-07-20 17:27:26 +0100

    ...

--- app/transaction/page.tsx
+++ app/transaction/page.tsx
@@ -1,20 +1,20 @@
 import TransactionList from '../components/TransactionList';
 import Wrapper from '../components/Wrapper'
-import { Suspense } from "react"; // Importez Suspense
+/* import { Suspense } from "react"; // Importez Suspense */
 
 const TransactionPage = async () => {
     return (
         <Wrapper >
             {/* Transactions List */}
             {/* Utilisation de Suspense pour charger les transactions de manière asynchrone */}
-            <Suspense fallback={
+           {/*  <Suspense fallback={
                 <div className="flex justify-center items-center py-10">
                     <span className="loading loading-spinner loading-lg text-primary"></span>
                     <span className="ml-4 font-bold text-primary">Chargement des transactions...</span>
                 </div>
-            }>
+            }> */}
                 <TransactionList /> {/* Le composant qui va chercher les données */}
-            </Suspense>
+            {/* </Suspense> */}
 
         </Wrapper>
     )
